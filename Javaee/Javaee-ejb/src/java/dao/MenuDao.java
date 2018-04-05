a810ef40a128bd4f4be2215540b8309aa57dbcc5
Zheng Liang <Zheng Liang@MSI>
2018-04-05 15:45:53 +0200

    test

---
+++ Javaee/Javaee-ejb/src/java/dao/MenuDao.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Zheng Liang
+ */
+@Stateless
+public class MenuDao implements MenuDaoLocal {
+
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+}
