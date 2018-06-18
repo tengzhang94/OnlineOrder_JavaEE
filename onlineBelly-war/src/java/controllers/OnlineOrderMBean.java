7f1e066103a4b62c6f89cedd368d279bcc976c2a
toon.vanmiert <toon.vanmiert@student.kuleuven.be>
2018-06-18 18:41:20 +0200

    added jsf layer

---
+++ onlineBelly-war/src/java/controllers/OnlineOrderMBean.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controllers;
+
+import belly.ejb.CourseOverviewBean;
+import belly.ejb.CustomerCredentialsBean;
+import belly.ejb.CustomerSessionBean;
+import belly.entities.Course;
+import belly.entities.Person;
+import belly.exceptions.InvalidCredentialsException;
+import belly.exceptions.NotUniqueCredentialsException;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.security.Principal;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.model.SelectItem;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author toon1
+ */
+@Named(value = "onlineOrderMBean")
+@SessionScoped
+public class OnlineOrderMBean implements Serializable {
+
+    @EJB
+    private CustomerSessionBean customerSessionBean;
+    @EJB
+    private CustomerCredentialsBean customerCredentialsBean;
+    @EJB
+    private CourseOverviewBean courseOverviewBean;
+
+
+    /**
+     * Creates a new instance of OnlineOrderMBean
+     */
+    public OnlineOrderMBean() {
+    }
+    
+     /**
+     *
+     * @return list of course objects to be displayed in the menu
+     */
+    public List<Course> getCourses()
+    {
+        return courseOverviewBean.getOverview();
+    }
+    
+    public void orderCourse()
+    {
+        //check if logged in
+        //case yes, add to order
+        //else redirect to login view
+    }
+    public void loginCustomer(String loginName,byte[] password)
+    {
+        try
+        {
+            customerSessionBean = customerCredentialsBean.loginCustomer(loginName, password);
+        }
+        catch (InvalidCredentialsException e)
+        {
+            //display msg to try again
+        }
+    }
+    public void registerCustomer(String nickName, String loginName,byte[] password)
+    {
+        try
+        {
+            customerSessionBean = customerCredentialsBean.registerCustomer(loginName, password, nickName);
+        }
+        catch (NotUniqueCredentialsException e)
+        {
+            //display msg to use other loginName
+        }
+    }
+    /**
+     * see if a user is logged in, otherwise signal that  
+     * @return the current customer
+     */
+    public Person getCustomer()
+    {
+        if (checkLoggedInUser())
+            return customerSessionBean.getCustomer();
+        else
+               //redirect to login page;
+        return null;    
+    }
+
+    private boolean checkLoggedInUser()
+    {
+        Principal loginUser = getLoggedInUser();
+        return (loginUser != null);
+    }
+
+    public String showCustomerName(Person customer)
+    {
+        return customerSessionBean.getCustomer().getName();
+    }
+
+    /**
+     *
+     * @return Principal of the logged-in user
+     */
+    private Principal getLoggedInUser()
+    {
+        HttpServletRequest request =
+                (HttpServletRequest) FacesContext.getCurrentInstance().
+                    getExternalContext().getRequest();
+        return request.getUserPrincipal();
+    }
+
+}
